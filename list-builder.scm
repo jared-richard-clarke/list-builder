05600023c8b4c5adb9efe4f2d743144512122e73
Jared Clarke <80301412+jared-richard-clarke@users.noreply.github.com>
2023-04-22 00:24:22 -0700

    Create list-builder.scm

---
+++ list-builder.scm
@@ -0,0 +1,38 @@
+(library (list-builder)
+         (export for-list)
+         (import (rnrs))
+         
+         (define-syntax for-list
+           (syntax-rules (<-)
+             [(_ expression [(x <- mx)])
+              (bind mx (lambda (x)
+                         (return expression)))]
+             [(_ expression [(x <- mx) (y <- my) ...])
+              (bind mx (lambda (x)
+                         (for-list expression [(y <- my) ...])))]
+             [(_ expression [(x <- mx)] predicate)
+              (bind mx (lambda (x)
+                         (if predicate
+                             (return expression)
+                             empty)))]
+             [(_ expression [(x <- mx) (y <- my) ...] predicate)
+              (bind mx (lambda (x)
+                         (for-list expression [(y <- my) ...] predicate)))]))
+
+         ;; === monad ===
+
+         (define return
+           (lambda (x) (list x)))
+
+         (define bind
+           (lambda (xs f)
+             (concat (map f xs))))
+
+         (define empty '())
+
+         ;; === utils ===
+
+         (define concat
+           (lambda (xs)
+             (fold-right append '() xs)))
+         )
