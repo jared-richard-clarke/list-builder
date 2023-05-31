102155357c6fc956b2482e6bcd568f97de2bab20
Jared Clarke <80301412+jared-richard-clarke@users.noreply.github.com>
2023-05-31 16:58:39 -0700

    Remove variable shadowing

--- test-monad.scm
+++ test-monad.scm
@@ -15,8 +15,8 @@
 
 ;; associativity: (m >>= f) >>= g = m >>= (\x -> f x >>= g)
 
-(let ([sub1 (lambda (x) (list (sub1 x)))]
-      [add1 (lambda (x) (list (add1 x)))])
+(let ([sub1-list (lambda (x) (list (sub1 x)))]
+      [add1-list (lambda (x) (list (add1 x)))])
   (assert equal?
-          (bind (bind (list 7) sub1) add1)
-          (bind (list 7) (lambda (x) (bind (sub1 x) add1)))))
+          (bind (bind (list 7) sub1-list) add1-list)
+          (bind (list 7) (lambda (x) (bind (sub1-list x) add1-list)))))
