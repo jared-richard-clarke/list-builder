74199f8074f7f4ded1ba3c76ba6b691f27b07d98
Jared Clarke <80301412+jared-richard-clarke@users.noreply.github.com>
2023-06-07 13:27:02 -0700

    Rename identifiers
    Rename "bind", "return", and "empty" to prevent namespace collisions.

--- list-builder.scm
+++ list-builder.scm
@@ -6,30 +6,30 @@
          (define-syntax for-list
            (syntax-rules (<-)
              [(_ expression [(x <- mx)])
-              (bind mx (lambda (x)
-                         (return expression)))]
+              (bind-list mx (lambda (x)
+                              (return-list expression)))]
              [(_ expression [(x <- mx) (y <- my) ...])
-              (bind mx (lambda (x)
-                         (for-list expression [(y <- my) ...])))]
+              (bind-list mx (lambda (x)
+                              (for-list expression [(y <- my) ...])))]
              [(_ expression [(x <- mx)] predicate)
-              (bind mx (lambda (x)
-                         (if predicate
-                             (return expression)
-                             empty)))]
+              (bind-list mx (lambda (x)
+                              (if predicate
+                                  (return-list expression)
+                                  empty-list)))]
              [(_ expression [(x <- mx) (y <- my) ...] predicate)
-              (bind mx (lambda (x)
-                         (for-list expression [(y <- my) ...] predicate)))]))
+              (bind-list mx (lambda (x)
+                              (for-list expression [(y <- my) ...] predicate)))]))
 
          ;; === monad ===
 
-         (define return
+         (define return-list
            (lambda (x) (list x)))
 
-         (define bind
+         (define bind-list
            (lambda (xs f)
              (concat (map f xs))))
 
-         (define empty '())
+         (define empty-list '())
 
          ;; === utils ===
 
