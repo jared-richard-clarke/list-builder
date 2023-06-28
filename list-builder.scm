ab33438a2f362070c1efb040e02a226e145882ea
Jared Clarke <80301412+jared-richard-clarke@users.noreply.github.com>
2023-06-28 13:38:26 -0700

    Reformat and rename

--- list-builder.scm
+++ list-builder.scm
@@ -5,35 +5,34 @@
          
          (define-syntax for-list
            (syntax-rules (<-)
-             [(_ expression [(x <- mx)])
-              (bind-list mx (lambda (x)
-                              (return-list expression)))]
-             [(_ expression [(x <- mx) (y <- my) ...])
-              (bind-list mx (lambda (x)
-                              (for-list expression [(y <- my) ...])))]
-             [(_ expression [(x <- mx)] predicate)
-              (bind-list mx (lambda (x)
-                              (if predicate
-                                  (return-list expression)
-                                  empty-list)))]
-             [(_ expression [(x <- mx) (y <- my) ...] predicate)
-              (bind-list mx (lambda (x)
-                              (for-list expression [(y <- my) ...] predicate)))]))
+             [(_ expression ([x <- mx]))
+              (bind mx (lambda (x)
+                         (return expression)))]
+             [(_ expression ([x <- mx] [y <- my] ...))
+              (bind mx (lambda (x)
+                         (for-list expression ([y <- my] ...))))]
+             [(_ expression ([x <- mx]) predicate)
+              (bind mx (lambda (x)
+                         (if predicate
+                             (return expression)
+                             empty)))]
+             [(_ expression ([x <- mx] [y <- my] ...) predicate)
+              (bind mx (lambda (x)
+                         (for-list expression ([y <- my] ...) predicate)))]))
 
          ;; === monad ===
 
-         (define return-list
+         (define return
            (lambda (x) (list x)))
 
-         (define bind-list
+         (define bind
            (lambda (xs f)
              (concat (map f xs))))
 
-         (define empty-list '())
-
-         ;; === utils ===
+         (define empty '())
 
          (define concat
            (lambda (xs)
              (fold-right append '() xs)))
+         
          )
