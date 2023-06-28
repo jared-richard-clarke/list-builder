5f4976c1a4fc1a38b56bf478188a2e9f3e3e612c
Jared Clarke <80301412+jared-richard-clarke@users.noreply.github.com>
2023-06-28 13:39:53 -0700

    Reformat

--- test-list-builder.scm
+++ test-list-builder.scm
@@ -5,9 +5,9 @@
 (define py-triple
   (lambda (n)
     (for-list (list x y z)
-              [(x <- (range 1 n))
-               (y <- (range x n))
-               (z <- (range y n))]
+              [[x <- (range 1 n)]
+               [y <- (range x n)]
+               [z <- (range y n)])
               (= (+ (sqr x) (sqr y))
                  (sqr z)))))
 
