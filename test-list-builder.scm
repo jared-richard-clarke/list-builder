817d6dd21e391a2895f9ddb5c9f37628e31e0381
Jared Clarke <80301412+jared-richard-clarke@users.noreply.github.com>
2023-10-17 16:59:41 -0700

    Add filter test

--- test-list-builder.scm
+++ test-list-builder.scm
@@ -2,6 +2,9 @@
         (list-builder)
         (utils))
 
+(define (filter f xs)
+  (yield x ([x <- xs]) (f x)))
+
 (define py-triple
   (lambda (n)
     (for-list (list x y z)
@@ -12,5 +15,7 @@
                  (sqr z)))))
 
 ;; === tests ===
+
+(assert equal? (filter even? '(1 2 3 4 5 6 7 8 9 10)) '(2 4 6 8 10))
 
 (assert equal? (py-triple 21) '((3 4 5) (5 12 13) (6 8 10) (8 15 17) (9 12 15) (12 16 20)))
