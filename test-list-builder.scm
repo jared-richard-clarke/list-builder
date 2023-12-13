0a71bf7023b106480963288d798eaeba58091f6a
Jared Clarke <80301412+jared-richard-clarke@users.noreply.github.com>
2023-12-13 05:38:49 -0800

    Replace "filter" with "count-to-fifteen"

--- test-list-builder.scm
+++ test-list-builder.scm
@@ -2,8 +2,14 @@
         (list-builder)
         (utils))
 
-(define (filter f xs)
-  (yield x ([x <- xs]) (f x)))
+(define binary '("0" "1"))
+
+(define count-to-fifteen
+  (yield (string-append bit-4 bit-3 bit-2 bit-1)
+         ([bit-4 <- binary]
+          [bit-3 <- binary]
+          [bit-2 <- binary]
+          [bit-1 <- binary])))
 
 (define (py-triple n)
   (yield (list x y z)
@@ -14,7 +20,6 @@
             (sqr z))))
 
 ;; === tests ===
-
-(assert equal? (filter even? '(1 2 3 4 5 6 7 8 9 10)) '(2 4 6 8 10))
+(assert equal? count-to-fifteen '("0000" "0001" "0010" "0011" "0100" "0101" "0110" "0111" "1000" "1001" "1010" "1011" "1100" "1101" "1110" "1111"))
 
 (assert equal? (py-triple 21) '((3 4 5) (5 12 13) (6 8 10) (8 15 17) (9 12 15) (12 16 20)))
